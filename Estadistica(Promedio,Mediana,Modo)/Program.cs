db15a15d2274c945266aa1bd2d571f795b698d03
Joaquin <Joaquin@DESKTOP-VNFEOJT>
2024-09-06 19:48:09 -0300

    pull

---
+++ Estadistica(Promedio,Mediana,Modo)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estadistica_Promedio_Mediana_Modo_
+{
+    internal class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            List<int> Valores = new List<int> {3,7,9,10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        }
+
+
+
+        static double calcularPromedio(List<int> valores)
+        {
+            double promedio = valores.Average();
+            return promedio;
+            
+        }
+
+        static double calcularMediana(List<int> valores)
+        {
+            valores.Sort();
+            int indice;
+            int mediana;
+            if(valores.Count%2 != 0)
+            {
+                indice = valores               
+            }
+            return;
+        }
+    }
+}
